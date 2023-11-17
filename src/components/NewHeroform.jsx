4816381e2c8b67662458e24b8e52dc27ea67d4a1
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-17 02:31:03 -0500

    css changes

--- src/components/NewHeroform.jsx
+++ src/components/NewHeroform.jsx
@@ -1,9 +1,9 @@
 // src/NewHeroForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../App.css'; // Import the shared styles
+// import '../App.css'; // Import the shared styles
 import '../formStyles.css'; // Import the form-specific styles
-import Navbar from './NavBar';
+
 
 const NewHeroForm = () => {
     const [heroData, setHeroData] = useState({
@@ -35,56 +35,56 @@ const NewHeroForm = () => {
     };
 
     return (
-
-        <div className="form-container">
-            <Navbar/>
-            <h1>Add a New Hero</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="alias">Alias:</label>
-                    <input
-                        type="text"
-                        id="alias"
-                        name="alias"
-                        value={heroData.alias}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="lastname">Last Name:</label>
-                    <input
-                        type="text"
-                        id="lastname"
-                        name="lastname"
-                        value={heroData.lastname}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="dob">Date of Birth:</label>
-                    <input
-                        type="date"
-                        id="dob"
-                        name="dob"
-                        value={heroData.dob}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <input
-                        type="checkbox"
-                        id="adult"
-                        name="adult"
-                        checked={heroData.adult}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="adult">Adult</label>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+        <div className='container'>
+            <div className="form-container">
+                <h1>Add a New Hero</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="alias">Alias:</label>
+                        <input
+                            type="text"
+                            id="alias"
+                            name="alias"
+                            value={heroData.alias}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="lastname">Last Name:</label>
+                        <input
+                            type="text"
+                            id="lastname"
+                            name="lastname"
+                            value={heroData.lastname}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="dob">Date of Birth:</label>
+                        <input
+                            type="date"
+                            id="dob"
+                            name="dob"
+                            value={heroData.dob}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="adult"
+                            name="adult"
+                            checked={heroData.adult}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="adult">Adult</label>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
