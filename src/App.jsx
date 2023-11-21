35a63f7fc31a015d0f27f6b211669090562b7e5e
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-21 06:51:47 -0500

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
 import Superhero_Profiles from './components/Superhero Profiles';
 import Navbar from './components/NavBar';
 import NewHeroForm from './components/NewHeroform';
+import SuperheroDetails from './components/SuperheroDetails';
+import EditHeroForm from './components/EditHeroForm';
 import './App.css'
 
 const App = () => {
@@ -10,9 +13,11 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Superhero_Profiles />} />
+          <Route path='/' element={<Home />} />
           <Route path='/list' element={<Superhero_Profiles />} />
           <Route path='/new' element={<NewHeroForm />} />
+          <Route path='/superhero/:key' element={<SuperheroDetails />} />
+          <Route path='/superhero/:key/edit' element={<EditHeroForm />} />
         </Routes>
       </Router>
     </>
