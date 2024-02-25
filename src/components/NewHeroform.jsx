1931f7c085a426b822a25693c47f0b951da3fc01
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2024-02-25 12:16:07 -0500

    minor changes

--- src/components/NewHeroform.jsx
+++ src/components/NewHeroform.jsx
@@ -1,5 +1,5 @@
 // src/NewHeroForm.js
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import '../App.css'; // Import the shared styles
 import '../formStyles.css'; // Import the form-specific styles
