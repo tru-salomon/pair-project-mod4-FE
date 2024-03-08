e59cf149b3b512ec26d99d3279c4c34c51b67ee1
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2024-03-07 22:41:21 -0500

    properly formated date

--- src/components/SuperheroCard.jsx
+++ src/components/SuperheroCard.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
 
 const SuperheroCard = ({ superhero }) => {
+
+    let dob = new Date(superhero.dob);
+    let superDob = dob.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    });
+
     return (
         <div className="superhero-card">
             <h2>{superhero.alias} </h2>
             <h2> {superhero.lastname}</h2>
-            <p>Date of Birth: {superhero.dob}</p>
+            <p>Date of Birth: {superDob}</p>
             <p>Adult: {superhero.adult ? 'Yes' : 'No'}</p>
         </div>
     );
